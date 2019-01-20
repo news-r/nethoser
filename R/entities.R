b482c3013dd75064f995aff4f6616a97956ec2bd
John Coene <jcoenep@gmail.com>
2019-01-20 17:24:00 +0100

    from to added

--- R/entities.R
+++ R/entities.R
@@ -1,9 +1,10 @@
 globalVariables(
     c(
-        ".",
+        "entity",
         "target",
         "uuid",
-        "doc"
+        "doc",
+        "."
     )
 )
 
@@ -12,7 +13,7 @@ globalVariables(
 #' Create network from entities
 #'
 #' @param data The data, as returned by \link[webhoser]{wh_collect}.
-#' @param entity Entity to build network.
+#' @param from,to Entities to build network.
 #'
 #' @examples
 #' data("webhoser")
@@ -28,32 +29,52 @@ globalVariables(
 #'   \item{\code{edges}: The \code{source}, \code{target}, and \code{n}umber of edges.}
 #' }
 #'
-#' @note The returned nodes and edges form an \emph{undirected} graph.
+#' @examples
+#' data("webhoser")
+#'
+#' webhoser %>%
+#'   net_entities(entities.persons)
+#'
+#' @details The returned nodes and edges form an \emph{undirected} graph.
 #'
 #' @export
-net_entities <- function(data, entity){
+net_entities <- function(data, from, to = NULL){
 
-    if(missing(data) || missing(entity))
-        stop("Missing data or entity", call. = FALSE)
+    if(missing(data) || missing(from))
+        stop("Missing data or from", call. = FALSE)
 
-    entity_enquo <- dplyr::enquo(entity)
+    from_enquo <- rlang::enquo(from)
+    to_enquo <- rlang::enquo(to)
+    has_to <- rlang::quo_is_null(to_enquo)
+    
+    if(has_to)
+        warning("Building co-mention graph", call. = FALSE)
 
     edges <- data %>%
-        select(
-            doc = uuid,
-            entity = !!entity_enquo
-        ) %>%
-        filter(entity != "") %>%
-        tidyr::separate_rows(entity, sep = ",") %>%
-        filter(!is.na(entity)) %>%
+        {
+            if(has_to)
+                select(., doc = uuid, from = !!from_enquo) %>% mutate(to = from)
+            else
+                select(., doc = uuid, from = !!from_enquo, to = !!to_enquo) 
+        } %>% 
+        distinct() %>% 
+        filter(from != "", to != "") %>%
+        tidyr::separate_rows(from, sep = ",") %>%
+        tidyr::separate_rows(to, sep = ",") %>%
+        distinct() %>% 
+        filter(!is.na(from), !is.na(to)) %>%
         split(.$doc) %>%
         purrr::map_df(function(x){
             tidyr::crossing(
-                source = x$entity,
-                target = x$entity
+                source = x$to,
+                target = x$from
             )
         }) %>%
         filter(target > source) %>%
+        mutate(
+            source = trimws(source),
+            target = trimws(target)
+        ) %>% 
         count(source, target, sort = TRUE)
 
     nodes <- bind_rows(
