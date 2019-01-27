bd39c9f75c8a97e08d9eb2cb81b12458cc3a4696
JohnCoene <jcoenep@gmail.com>
2019-01-27 12:42:39 +0100

    plot colors

--- R/plot.R
+++ R/plot.R
@@ -1,34 +1,38 @@
 #' Visualise
-#' 
+#'
 #' Visualise nethoser network.
-#' 
+#'
 #' @param graph A list as returned by \code{\link{connect}}.
-#' 
-#' @examples 
+#'
+#' @examples
 #' data("webhoser")
-#' 
+#'
 #' # make network
 #' webhoser %>%
-#'   connect(thread.site, entities.persons) %>% 
+#'   connect(thread.site, entities.persons) %>%
 #'   visualize()
-#' 
+#'
 #' @rdname viz
 #' @export
 visualize <- function(graph){
-  nodes <- graph$nodes %>% 
-    select(label = entity, size = n) %>% 
-    mutate(id = label) 
-  
-  edges <- graph$edges %>% 
-    select(source, target, weight = n) %>% 
+  nodes <- graph$nodes %>%
+    select(label = entity, size = n) %>%
+    mutate(id = label)
+
+  edges <- graph$edges %>%
+    select(source, target, weight = n) %>%
     mutate(id = 1:dplyr::n())
-    
-  sigmajs() %>% 
-    sg_nodes(nodes, id, label, size) %>% 
-    sg_edges(edges, id, source, target, weight) %>% 
-    sg_layout() %>% 
-    sg_cluster() %>% 
-    sg_drag_nodes()  %>% 
+
+  sigmajs() %>%
+    sg_nodes(nodes, id, label, size) %>%
+    sg_edges(edges, id, source, target, weight) %>%
+    sg_layout() %>%
+    sg_cluster(
+      colors = c(
+        "#05143a", "#acadaf"
+      )
+    ) %>%
+    sg_drag_nodes()  %>%
     sg_neighbours()
 }
 
